22a637b1da03890688689f105b286bc57fb21b47
robjuz <robjuz@gmail.com>
2016-09-09 12:19:01 +0200

    Update jscroll.js

--- jscroll.js
+++ jscroll.js
@@ -1,14 +1,18 @@
-(function($) {
-    $.fn.jScroll = function(e) {
+/**
+ * Created by robjuz on 09.09.16.
+ */
+(function ($) {
+    $.fn.jScroll = function (e) {
         var f = $.extend({}, $.fn.jScroll.defaults, e);
-        return this.each(function() {
+        return this.each(function () {
             var element = $(this);
             element.initialOffsetTop = element.offset().top;
             function scroll() {
-                if ($(window).scrollTop() >= (element.initialOffsetTop - f.top)) {
-                    element.offset({top: $(window).scrollTop() + f.top});
+                if (($(window).scrollTop() >= (element.initialOffsetTop - f.top)) &&
+                    ($(window).width() >= f.min_width)) {
+                    element.stop().animate({'margin-top':$(window).scrollTop()-element.initialOffsetTop + f.top});
                 } else {
-                    element.offset({top: element.initialOffsetTop});
+                    element.stop().animate({'margin-top':0});
                 }
             };
             $('document').ready(scroll);
@@ -16,6 +20,7 @@
         });
     };
     $.fn.jScroll.defaults = {
-        top: 10
+        top: 10,
+        min_width: 768
     };
 })(jQuery);
